f4f1c3226885ffc0c093e9da08ad7e18337fc2f8
Okky Hendriansyah <okky.htf@gmail.com>
2018-09-02 15:39:09 +0700

    Ready to trial run for deploying the cluster.

--- scripts/pvs_10Gsize.sh
+++ scripts/pvs_10Gsize.sh
@@ -2,5 +2,25 @@
 
 export GUID=`hostname|awk -F. '{print $2}'`
 export volsize="10Gi"
+export nfspath="/srv/nfs/user-vols"
 
-????
+mkdir -p ./pv
+
+for i in 1 .. 25
+do
+   cat << EOF > ./pv/pv$i.yaml
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: pv$i-volume 
+spec:
+  capacity:
+    storage: $volsize
+  accessModes:
+  - ReadWriteOnce 
+  nfs: 
+    path: $nfspath/pv$i
+    server: support1.$GUID.internal
+  persistentVolumeReclaimPolicy: Recycle 
+EOF
+done
