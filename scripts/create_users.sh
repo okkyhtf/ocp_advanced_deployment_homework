94d3e6d419d89292f853f3f88754793180b958e8
Okky Hendriansyah <okky.htf@gmail.com>
2018-09-03 03:53:13 +0700

    Almost ready

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -7,11 +7,3 @@ htpasswd -b $pwdFile andrew $genericPwd
 htpasswd -b $pwdFile amy $genericPwd
 htpasswd -b $pwdFile brian $genericPwd
 htpasswd -b $pwdFile betty $genericPwd
-
-oc login -u system:admin
-
-oc adm groups new alphacorp andrew amy
-oc adm groups new betacorp brian betty
-
-oc label group/alphacorp client=alpha
-oc label group/betacorp client=beta 
