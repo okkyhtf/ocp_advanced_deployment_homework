954c45bd715462c5d1c1965852c481f22197a62b
Okky Hendriansyah <okky.htf@gmail.com>
2018-09-02 17:48:51 +0000

    Move Jenkins related things to Shell script.

--- scripts/jenkins_persistent.sh
+++ scripts/jenkins_persistent.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-# Create new project specific for smoke test run
+# Create new project specific for Jenkins persistent deployment and CI/CD
 oc new-project cicd
 
-# Verify the cluster deployment by installing sample NodeJS app
+# Install Jenkins with persistent storage
 oc new-app jenkins-persistent
+
+# Create dev, test, and prod environments
+oc new-project cicd-dev
+oc new-project cicd-test
+oc new-project cicd-prod
+
+# Give jenkins service account from cicd project privilage to edit dev, test, and prod environments
+oc adm policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n cicd-dev
+oc adm policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n cicd-test
+oc adm policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n cicd-prod
+
