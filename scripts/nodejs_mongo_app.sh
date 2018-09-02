d1578d21a0d0d8e7fdea97d7b2c8009dc253ce5f
root <root@bastion.a739.example.opentlc.com>
2018-09-02 13:35:23 +0000

    Add NodeJS smoke test

--- scripts/nodejs_mongo_app.sh
+++ scripts/nodejs_mongo_app.sh
@@ -7,29 +7,3 @@ oc new-project smoke-test
 
 # Verify the cluster deployment by installing sample NodeJS app
 oc new-app nodejs-mongo-persistent
-
-# Follow the deployment status
-while true
-do
-  sleep 1m
-  dc=`oc status | grep deployed | wc -l`
-  if [$dc == "2"] then
-    break
-  else
-  fi
-done
-
-# Expose route to public
-url="http://nodejs-mongo-persistent-smoke-test.apps.$GUID.example.opentlc.com"
-success=curl $url
-
-if $success then
-  echo "Smoke test successfully deployed"
-else
-  echo "Smoke test failed to be deployed"
-fi
-
-# Delete the sample project
-oc delete project smoke-test
-
-exit $success
