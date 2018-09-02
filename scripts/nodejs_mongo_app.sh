62f41c5579626f11b3ff86560560c84c1cb40644
Okky Hendriansyah <okky.htf@gmail.com>
2018-09-02 20:19:50 +0700

    Add smoke test script

--- scripts/nodejs_mongo_app.sh
+++ scripts/nodejs_mongo_app.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export GUID=`hostname|awk -F. '{print $2}'`
+
 # Create new project specific for smoke test run
 oc new-project smoke-test
 
@@ -7,7 +9,27 @@ oc new-project smoke-test
 oc new-app nodejs-mongo-persistent
 
 # Follow the deployment status
-oc status -v
+while true
+do
+  sleep 1m
+  dc=`oc status | grep deployed | wc -l`
+  if [$dc == "2"] then
+    break
+  else
+  fi
+done
+
+# Expose route to public
+url="http://nodejs-mongo-persistent-smoke-test.apps.$GUID.example.opentlc.com"
+success=curl $url
+
+if $success then
+  echo "Smoke test successfully deployed"
+else
+  echo "Smoke test failed to be deployed"
+fi
 
 # Delete the sample project
-#oc delete project smoke-test
+oc delete project smoke-test
+
+exit $success
