1381fc5f28d5631bba34e289992cace88d699b9b
okky hendriansyah tri firgantoro <otrifirg-redhat.com@bastion.727d.example.opentlc.com>
2018-09-03 05:05:48 +0000

    tidying up

--- scripts/set_labels.sh
+++ scripts/set_labels.sh
@@ -5,5 +5,5 @@ oc login -u system:admin
 oc adm groups new alphacorp andrew amy
 oc adm groups new betacorp brian betty
 
-oc label group/alphacorp client=alpha
-oc label group/betacorp client=beta 
+oc label group alphacorp client=alpha
+oc label group betacorp client=beta 
